6a45a07895c19343ee8e616dc59bf5271c69c08f
Iota-Rho-Stack <Jack.Russo88@spsmail.cuny.edu>
2020-11-17 21:33:21 +0000

    Edit.

--- Installation/Data/partitions.r
+++ Installation/Data/partitions.r
@@ -1,4 +1,4 @@
-# Initialize Data Partitions
+## Initialize Data Partitions
 
 # Load magrittr.
 base::library(magrittr)
@@ -8,79 +8,41 @@ if(base::dir.exists('Data')){
   base::unlink(x = 'Data', recursive = T)
 }
 
-if(!sparklyr::spark_install_find()$installed){
-  sparklyr::spark_install(version = "2.1.0")
-  if(!base::dir.exists('Data')){
-    base::dir.create('Data')
-    ships_sdf <- sparklyr::spark_read_csv(
-      sc = base::source(file = 'Spark/connect.r')$value,
-      name = "ships_sdf",
-      path = 'Raw/raw.csv'
-    )
-    base::print('\nCreate List of Ship Types.')
-    ships_sdf %>%
-      dplyr::select("ship_type") %>%
-      dplyr::distinct() %>%
-      sparklyr::sdf_coalesce(partitions = 1) %>%
-      sparklyr::spark_write_csv(path = 'Data/ShipType')
-    base::print('\nCreate Ship Directory.')
-    ships_sdf %>%
-      dplyr::select(c("SHIPNAME","SHIP_ID","ship_type")) %>%
-      dplyr::distinct() %>%
-      dplyr::mutate(SHIP_ID_str = as.character(SHIP_ID)) %>%
-      dplyr::select(-c("SHIP_ID")) %>%
-      sparklyr::sdf_coalesce(partitions = 1) %>%
-      sparklyr::spark_write_csv(path = 'Data/ShipName&ID&Type')
-    base::print('n\Partitioning Data... ')
-    for(TYPE in c(base::unlist(source('Functions/shiptype_tbl.r')$value()))){
-      for(NAME in (source('Functions/selector_tbl.r')$value() %>% dplyr::filter(ship_type == TYPE))$SHIPNAME){
-        for(ID in (source('Functions/selector_tbl.r')$value() %>% dplyr::filter(SHIPNAME == NAME))$SHIP_ID){
-          ships_sdf %>%
-            dplyr::filter(SHIPNAME == NAME) %>%
-            sparklyr::sdf_coalesce(partitions = 1) %>%
-            sparklyr::spark_write_csv(path = base::paste('Data/Partition',TYPE,ID,sep = '/'), mode = 'append')
-        }
+# Build Data Folder
+if(!base::dir.exists('Data')){
+  base::dir.create('Data')
+  ships_sdf <- sparklyr::spark_read_csv(
+    sc = base::source(file = 'Spark/connect.r')$value,
+    name = "ships_sdf",
+    path = 'Raw/raw.csv'
+  )
+  base::print('Create List of Ship Types.')
+  ships_sdf %>%
+    dplyr::select("ship_type") %>%
+    dplyr::distinct() %>%
+    sparklyr::sdf_coalesce(partitions = 1) %>%
+    sparklyr::spark_write_csv(path = 'Data/ShipType')
+  base::print('Create Ship Directory.')
+  ships_sdf %>%
+    dplyr::select(c("SHIPNAME","SHIP_ID","ship_type")) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(SHIP_ID_str = as.character(SHIP_ID)) %>%
+    dplyr::select(-c("SHIP_ID")) %>%
+    sparklyr::sdf_coalesce(partitions = 1) %>%
+    sparklyr::spark_write_csv(path = 'Data/ShipName&ID&Type')
+  base::print('Partitioning Data... ')
+  for(TYPE in c(base::unlist(source('Functions/shiptype_tbl.r')$value()))){
+    for(NAME in (source('Functions/selector_tbl.r')$value() %>% dplyr::filter(ship_type == TYPE))$SHIPNAME){
+      for(ID in (source('Functions/selector_tbl.r')$value() %>% dplyr::filter(SHIPNAME == NAME))$SHIP_ID){
+        ships_sdf %>%
+          dplyr::filter(SHIPNAME == NAME) %>%
+          sparklyr::sdf_coalesce(partitions = 1) %>%
+          sparklyr::spark_write_csv(path = base::paste('Data/Partition',TYPE,ID,sep = '/'), mode = 'append')
       }
-      base::print('.')
     }
+    base::print('.')
   }
-  base::print('n\Partition Complete.')
-} else{
-  if(!base::dir.exists('Data')){
-    base::dir.create('Data')
-    ships_sdf <- sparklyr::spark_read_csv(
-      sc = base::source(file = 'Spark/connect.r')$value,
-      name = "ships_sdf",
-      path = 'Raw/raw.csv'
-    )
-    base::print('\nCreate List of Ship Types.')
-    ships_sdf %>%
-      dplyr::select("ship_type") %>%
-      dplyr::distinct() %>%
-      sparklyr::sdf_coalesce(partitions = 1) %>%
-      sparklyr::spark_write_csv(path = 'Data/ShipType')
-    base::print('\nCreate Ship Directory.')
-    ships_sdf %>%
-      dplyr::select(c("SHIPNAME","SHIP_ID","ship_type")) %>%
-      dplyr::distinct() %>%
-      dplyr::mutate(SHIP_ID_str = as.character(SHIP_ID)) %>%
-      dplyr::select(-c("SHIP_ID")) %>%
-      sparklyr::sdf_coalesce(partitions = 1) %>%
-      sparklyr::spark_write_csv(path = 'Data/ShipName&ID&Type')
-    base::print('n\Partitioning Data... ')
-    for(TYPE in c(base::unlist(source('Functions/shiptype_tbl.r')$value()))){
-      for(NAME in (source('Functions/selector_tbl.r')$value() %>% dplyr::filter(ship_type == TYPE))$SHIPNAME){
-        for(ID in (source('Functions/selector_tbl.r')$value() %>% dplyr::filter(SHIPNAME == NAME))$SHIP_ID){
-          ships_sdf %>%
-            dplyr::filter(SHIPNAME == NAME) %>%
-            sparklyr::sdf_coalesce(partitions = 1) %>%
-            sparklyr::spark_write_csv(path = base::paste('Data/Partition',TYPE,ID,sep = '/'), mode = 'append')
-        }
-      }
-      base::print('.')
-    }
-  }
-  base::print('n\Partition Complete.')
+  base::print('Partition Complete.')
 }
 
 # Spark disconnect.
--- setupApp.r
+++ setupApp.r
@@ -1,5 +1,8 @@
 # Install Dependencies.
 source('Installation/Dependencies/install_dependencies.r')
 
+# Install Spark
+source('Installation/Spark/install_spark.r')
+
 # Initialize Data Partitions.
 source('Installation/Data/partitions.r')
