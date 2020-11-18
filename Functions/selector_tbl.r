a729391d04c2f8a7fdf024f5faa6edf2a267323c
Iota-Rho-Stack <Jack.Russo88@spsmail.cuny.edu>
2020-11-18 02:12:35 +0000

    QWERTYASDF

--- Functions/selector_tbl.r
+++ Functions/selector_tbl.r
@@ -4,15 +4,15 @@ function(){
                                    list.files(path = list.dirs(path = 'Data')[-1][list.dirs(path = 'Data')[-1] == 'Data/ShipName&ID&Type'])[2])) %>%
     dplyr::mutate(SHIP_ID = SHIP_ID_str %>% 
                     stringr::str_replace_all(pattern = ' ',replacement = '') %>%
-                    stringr::str_replace_all(pattern = '0',replacement = ')') %>%
-                    stringr::str_replace_all(pattern = '1',replacement = '!') %>%
-                    stringr::str_replace_all(pattern = '2',replacement = '@') %>%
-                    stringr::str_replace_all(pattern = '3',replacement = '#') %>%
-                    stringr::str_replace_all(pattern = '4',replacement = '$') %>%
-                    stringr::str_replace_all(pattern = '5',replacement = '%') %>%
-                    stringr::str_replace_all(pattern = '6',replacement = '^') %>%
-                    stringr::str_replace_all(pattern = '7',replacement = '&') %>%
-                    stringr::str_replace_all(pattern = '8',replacement = '*') %>%
-                    stringr::str_replace_all(pattern = '9',replacement = '(')) %>%
+                    stringr::str_replace_all(pattern = '0',replacement = 'Q') %>%
+                    stringr::str_replace_all(pattern = '1',replacement = 'W') %>%
+                    stringr::str_replace_all(pattern = '2',replacement = 'E') %>%
+                    stringr::str_replace_all(pattern = '3',replacement = 'R') %>%
+                    stringr::str_replace_all(pattern = '4',replacement = 'T') %>%
+                    stringr::str_replace_all(pattern = '5',replacement = 'Y') %>%
+                    stringr::str_replace_all(pattern = '6',replacement = 'A') %>%
+                    stringr::str_replace_all(pattern = '7',replacement = 'S') %>%
+                    stringr::str_replace_all(pattern = '8',replacement = 'D') %>%
+                    stringr::str_replace_all(pattern = '9',replacement = 'F')) %>%
     dplyr::select(-c("SHIP_ID_str"))
 }
